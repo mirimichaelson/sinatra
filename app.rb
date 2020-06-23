4f0c676ba30ed80a7f553e622a511c1acee94161
Miri Michaelson <miriammichaelson@gmail.com>
2020-06-23 17:12:35 +0100

    Altered named_cat to respond to post rather than get

--- app.rb
+++ app.rb
@@ -15,12 +15,15 @@ end
 
 get '/random-cat' do
   @name = ["Jameson", "Reginald", "Ada"].sample
-  erb(:index)
+  erb :index
 end
 
-get '/named-cat' do
+get '/cat-form' do
+  erb :cat_form
+end
+
+post '/named-cat' do
   p params[:name]
-  p params[:age].to_i
   @name = params[:name]
-  erb(:index)
+  erb :index
 end
